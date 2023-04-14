c9a0b33f78b6824fbc378aa8bb8360b276559301
Kristiyan <bkristiqn7@gmail.com>
2023-04-15 00:00:05 +0300

    Add post validation

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -29,26 +29,11 @@ exports.validateRegister = () => [
         .custom((value, { req }) => value === req.body.password).withMessage('Passwords don\'t match'),
 ];
 
-/*
 // Post controller
-exports.validateReview = () => [
-    body('title').trim()
-        .notEmpty().withMessage('Title is required')
-        .isLength({ min: 2 }).withMessage('Title should be at least 2 characters'),
-    body('author').trim()
-        .notEmpty().withMessage('Author is required')
-        .isLength({ min: 5 }).withMessage('Author should be at least 5 characters'),
-    body('genre').trim()
-        .notEmpty().withMessage('Genre is required')
-        .isLength({ min: 5 }).withMessage('Genre should be at least 5 characters'),
-    body('stars').trim()
-        .notEmpty().withMessage('Stars are required')
-        .isNumeric({ min: 1, max: 5 }).withMessage('Stars must be a number between 1 and 5'),
+exports.validatePost = () => [
+    body('message').trim()
+        .notEmpty().withMessage('Message is required'),
     body('image').trim()
         .notEmpty().withMessage('Image is required')
-        .matches(/^https?:\/\/.*$/).withMessage('Invalid image url'),
-    body('review')
-        .notEmpty().withMessage('Review is required')
-        .isLength({ min: 10 }).withMessage('Review should be at least 10 characters'),
+        .matches(/^https?:\/\/.*$/).withMessage('Invalid image format'),
 ];
-*/
