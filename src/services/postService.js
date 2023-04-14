8b65f82be160e35d9eeafe2633ea28e98807452b
Kristiyan <bkristiqn7@gmail.com>
2023-04-15 02:37:12 +0300

    fix getAllPosts return value and add createPost function

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,3 +1,30 @@
 const Post = require("../models/Post");
+const { getUser } = require("./userService");
 
-exports.getAllPosts = () => Post.find().populate('owner', 'name surname profilePicture');
+exports.getAllPosts = () => Post.find().select('-likes -comments -__v').populate('owner', 'name surname profilePicture');
+
+exports.createPost = async (postData, owner) => {
+    // Get user
+    const user = await getUser(owner);
+
+    // Create post
+    const post = await Post.create({ ...postData, owner });
+
+    // Add post to user posts
+    user.posts.push(post._id);
+
+    // Return post and owner
+    return {
+        _id: post._id,
+        message: post.message,
+        image: post.image,
+        createdAt: post.createdAt,
+        updatedAt: post.updatedAt,
+        owner: {
+            _id: user._id,
+            name: user.name,
+            surname: user.surname,
+            profilePicture: user.profilePicture,
+        },
+    };
+}
